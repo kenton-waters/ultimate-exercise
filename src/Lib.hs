d6c34912bce099e6dcad8e94a69b09aae99d8039
Kenton Waters <kenton.a.waters@gmail.com>
2025-01-11 22:40:38 -0600

    invoke lilypond

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,10 @@
 module Lib
-    ( someFunc
+    ( invokeLilyPond
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Process
+
+type FileName = String
+
+invokeLilyPond :: FileName -> IO ()
+invokeLilyPond lyFileName = callCommand $ "lilypond --output 'output/' output/lilypond/" ++ lyFileName
